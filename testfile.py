0c43bee6a1118759bc91376087dcda92c3b3beac
Tiffany Tung <Tiff@wireless-10-145-85-233.public.utexas.edu>
2019-12-04 16:48:34 -0600

    test commit

---
+++ testfile.py
@@ -0,0 +1,5 @@
+print('this is my test file before I build my website')
+git init #initialize git respository in your home directory
+git config --global user.username tiffany86225 #update this with your actual github username
+git add testfile.py #replace testfile.py with any file in your home directory you want to upload: 
+git commit -m "test commit" #this commits the file and adds a message (required)
